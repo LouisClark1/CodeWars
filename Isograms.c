b8f6c958bc0d51c1a9892fc82a9db57e0c67304c
LouisClark1 <louis.clark2410@gmail.com>
2023-03-10 11:44:27 +0000

    completed Isogram task

---
+++ Isograms.c
@@ -0,0 +1,39 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+bool IsIsogram (const char *string);
+
+
+
+
+
+int main (){
+
+    bool result;
+    result = IsIsogram("Aa");
+    printf("%s\n", result ? "true" : "false");
+
+    printf("True means there are no matching characters\n\n");
+
+    return 0;
+}
+
+
+bool IsIsogram (const char *string){
+    unsigned int charCountOuter = 0;
+    unsigned int charCountInner = 0;
+
+    while (*(string + charCountOuter) != '\0'){
+        charCountInner = 0;
+        while (*(string + charCountInner) != '\0'){
+            if ( (tolower(*(string + charCountOuter)) == tolower(*(string + charCountInner)))
+                && 
+                (charCountInner != charCountOuter) ){
+                    return 0;
+            }
+            charCountInner++;
+        }
+        charCountOuter++;
+    }
+    return 1;
+}
